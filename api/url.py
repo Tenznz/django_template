3db3f3b9dce4f52ece9b88cd8a72341876a5d601
tenznz <dhugkar@gmail.com>
2022-05-26 16:44:57 +0530

    Added: TemplateHTMLRender

--- api/url.py
+++ api/url.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    # path('signup', views.StudentForm.as_view(), name='student_form')
+    # path('', views.index, name='index'),
+    path('signup', views.UserAPI.as_view(), name='user-list'),
+    path('user_details/<int:pk>', views.UserDetails.as_view(), name='user_details'),
+    path('register', views.UserDetails.as_view(), name='create_user')
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,29 +1,43 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect
+from rest_framework.generics import get_object_or_404
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .forms import UserForm
+from .models import User
+from .serializers import UserSerializer
 
 
-def index(request):
-    try:
-        form = UserForm()
-        if request.method == 'POST':
-            print(request.POST)
-            form = UserForm(request.POST)
-            if form.is_valid():
-                form.save()
-        context = {'form': form}
-        return render(request, 'index.html', context)
-    except Exception as e:
-        return HttpResponse(request, str(e))
+class UserAPI(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'index.html'
+
+    def get(self, request):
+        queryset = User.objects.all()
+        return Response({'users': queryset})
+
+    # def post(self, request):
+    #     serializer = UserSerializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     if not serializer.is_valid():
+    #         return Response({'serializer': serializer})
+    #     serializer.save()
+    #     return redirect('register')
 
 
-# class StudentForm(APIView):
-#     def get(self, request):
-#         try:
-#             form = UserForm()
-#             context = {'form': form}
-#             return render(request, 'index.html', context)
-#         except Exception as e:
-#             return HttpResponse(request, str(e))
+class UserDetails(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'register.html'
+
+    def get(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user)
+        return Response({'serializer': serializer, 'users': user})
+
+    def put(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if not serializer.is_valid():
+            return Response({'serializer': serializer, 'users': user})
+        serializer.save()
+        return redirect('register')
